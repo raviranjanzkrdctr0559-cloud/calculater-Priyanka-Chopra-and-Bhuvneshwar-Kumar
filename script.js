578940b8b42bdb3eac2c23814703f945a91da8b7
raviranjan kumar <raviranjanzkrdctr0559@gmail.com>
2026-07-07 22:25:45 +0530

    calculater

--- script.js
+++ script.js
@@ -200,3 +200,77 @@ V3 me Change PIN aur Security add hogi.`
 );
 
 };
+const modal = document.getElementById("settingsModal");
+const closeModalBtn = document.getElementById("closeModalBtn");
+const changePinBtn = document.getElementById("changePinBtn");
+const resetPinBtn = document.getElementById("resetPinBtn");
+
+settingsBtn.onclick = () => {
+    modal.style.display = "flex";
+};
+
+closeModalBtn.onclick = () => {
+    modal.style.display = "none";
+};
+
+resetPinBtn.onclick = () => {
+
+    let code = prompt("Enter Secret Reset Code");
+
+    if(code === "9661"){
+
+        localStorage.removeItem("vaultPin");
+
+        savedPin = null;
+
+        alert("PIN Reset Successfully");
+
+        modal.style.display = "none";
+
+    }else{
+
+        alert("Wrong Secret Code");
+
+    }
+
+};
+
+changePinBtn.onclick = () => {
+
+    if(savedPin === null){
+
+        alert("No PIN Set");
+
+        return;
+
+    }
+
+    let oldPin = prompt("Enter Old PIN");
+
+    if(oldPin !== savedPin){
+
+        alert("Wrong PIN");
+
+        return;
+
+    }
+
+    let newPin = prompt("Enter New PIN");
+
+    if(!/^\d{4}$/.test(newPin)){
+
+        alert("PIN must be 4 digits");
+
+        return;
+
+    }
+
+    savedPin = newPin;
+
+    localStorage.setItem("vaultPin", savedPin);
+
+    alert("PIN Changed Successfully");
+
+    modal.style.display = "none";
+
+};
