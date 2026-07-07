1657c71a229146e14c861e298bd53e565c5333d6
raviranjan kumar <raviranjanzkrdctr0559@gmail.com>
2026-07-07 23:07:25 +0530

    calculater

--- js/photos.js
+++ js/photos.js
@@ -27,3 +27,46 @@ async function uploadPhoto(file){
     alert("📷 Photo Saved Successfully");
 
 }
+async function loadPhotos(){
+
+    const gallery = document.getElementById("photoGallery");
+
+    const files = await getAllFiles();
+
+    const photos = files.filter(file => file.type === "photo");
+
+    gallery.innerHTML = "";
+
+    if(photos.length===0){
+
+        gallery.innerHTML="<p>No Photos Yet</p>";
+
+        return;
+
+    }
+
+    photos.forEach(photo=>{
+
+        const url = URL.createObjectURL(photo.blob);
+
+        const card = document.createElement("div");
+
+        card.className="photo-card";
+
+        card.innerHTML=`
+
+            <img src="${url}">
+
+            <div class="photo-name">
+
+                ${photo.name}
+
+            </div>
+
+        `;
+
+        gallery.appendChild(card);
+
+    });
+
+}
